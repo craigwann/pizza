16fe8d345265930e76b1ba97e9256648fe073d27
craigwann <craigwann@me.com>
2018-06-15 13:51:04 -0700

    add checkbox form to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,39 @@
-function Pizza(size, toppings, myPrice) {
+//business END
+function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 };
 
-Pizza.prototype.cost = function() {
-  var toppingPrice = this.toppings.lenghth;
-  var sizePrice = 10;
-  var total = toppingPrice + sizePrice;
-  return totalPrice;
-};
-
 mine = new Pizza("medium", ["pepperoni", "mushrooms", "olives"] );
 
-console.log(mine.cost());
+function sizePrice(size) {
+  if (size ==="large") {
+    return 15;
+  } else if (size ==="medium") {
+    return 10
+  } else {
+    return 5
+  }
+};
+
+Pizza.prototype.cost = function() {
+  var toppingPrice = this.toppings.lenghth * 2;
+  var sizeCost = sizePrice(this.size);
+  var totalPrice = toppingPrice + sizeCost;
+  return Number(totalPrice);
+};
+// console.log(mine.cost());
+
+
+//front END
+$(document).ready(function(){
+  $("form#pizza-toppings").submit(function(event){
+    event.preventDefault();
+    $("#results").show();
+    $("input:checkbox[name=pizza-toppings]:checked").each(function(){
+      var pizzaToppings = $(this).val();
+      $('#result').append(pizzaToppings + "<br>");
+    });
+    // $('#OrderForm').hide();
+  });
+});
