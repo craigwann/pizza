193dfa8c90dc969bae7624ef6d9fa76389c806a9
craigwann <craigwann@me.com>
2018-06-15 16:33:01 -0700

    fixing broken .cost() prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,11 @@ function Pizza(size, toppings) {
   this.toppings = toppings;
 };
 
-mine = new Pizza("medium", ["pepperoni", "mushrooms", "olives"] );
 
 function sizePrice(size) {
-  if (size ==="large") {
+  if (size ==="Large") {
     return 15;
-  } else if (size ==="medium") {
+  } else if (size ==="Medium") {
     return 10
   } else {
     return 5
@@ -19,8 +18,8 @@ function sizePrice(size) {
 Pizza.prototype.cost = function() {
   var toppingPrice = this.toppings.lenghth * 2;
   var sizeCost = sizePrice(this.size);
-  var totalPrice = toppingPrice + sizeCost;
-  return Number(totalPrice);
+  var totalCost = toppingPrice + sizeCost;
+  return totalCost;
 };
 // console.log(mine.cost());
 
@@ -29,15 +28,26 @@ Pizza.prototype.cost = function() {
 $(document).ready(function(){
   $("form#OrderForm").submit(function(event){
     event.preventDefault();
+
     $("#result").show();
+
+    var toppingArray = [];
 
     var sizeInput = $("input:radio[name=pizzaSize]:checked").val();
     $('#result').append('<h3>' + sizeInput + '</h3>' + "<br>");
 
     $("input:checkbox[name=pizza-toppings]:checked").each(function(){
-      var pizzaOrder = $(this).val();
-      $('#result').append(pizzaOrder + "<br>");
+      var toppingInput = $(this).val();
+      $('#result').append(toppingInput + "<br>");
+      toppingArray.push(toppingInput);
     });
+
+
+    var order = new Pizza(sizeInput, toppingArray);
+    var total = order.cost();
+
+    $('#result').append('<h3>' + total + '</h3>' + "<br>");
+
     $('#OrderForm').hide();
   });
 });
